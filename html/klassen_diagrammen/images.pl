9965a97c8d76f76119dfad2a5b668ba3bef64cb6
Dagmar Hofman (Kantoor) <dragomir@xs4all.nl>
2022-10-04 11:53:58 +0200

    Klassen diagrammen PDF en HTML toegevoegd.

---
+++ html/klassen_diagrammen/images.pl
@@ -0,0 +1,37 @@
+# LaTeX2HTML 2021.2 (Released July 1, 2021)
+# Associate images original text with physical files.
+
+
+$key = q/-;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ STYLE="height: 2.01ex; vertical-align: -0.31ex; " SRC="|."$dir".q|img2.svg"
+ ALT="$-$">|; 
+
+$key = q/sim;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ STYLE="height: 1.31ex; vertical-align: -0.12ex; " SRC="|."$dir".q|img3.svg"
+ ALT="$\sim$">|; 
+
+$key = q/{minted}{Java}+berekenBTW(bedrag:Double):Double{minted};AAT/;
+$cached_env_img{$key} = q|<IMG
+ STYLE="height: 0.46ex; vertical-align: -0.12ex; " SRC="|."$dir".q|img5.svg"
+ ALT="\begin{minted}{Java}
++berekenBTW(bedrag: Double): Double
+\end{minted}">|; 
+
+$key = q/{minted}{c}[toegang]naam([parameterlijst])[:resultaattype]{minted};AAT/;
+$cached_env_img{$key} = q|<IMG
+ STYLE="height: 0.46ex; vertical-align: -0.12ex; " SRC="|."$dir".q|img4.svg"
+ ALT="\begin{minted}{c}
+[toegang] naam([parameterlijst]) [: resultaattype]
+\end{minted}">|; 
+
+$key = q/{minted}{java}[toegang]naam[:type][=waarde]{minted};AAT/;
+$cached_env_img{$key} = q|<IMG
+ STYLE="height: 0.46ex; vertical-align: -0.12ex; " SRC="|."$dir".q|img1.svg"
+ ALT="\begin{minted}{java}
+[toegang] naam[: type] [= waarde]
+\end{minted}">|; 
+
+1;
+
